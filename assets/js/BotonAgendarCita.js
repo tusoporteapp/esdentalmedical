a643a72c1c8122e7c89658907325448279bb79e1
TuSoporteApp <tusoporte.app@gmail.com>
2025-01-03 19:04:37 -0500

    lll

--- assets/js/BotonAgendarCita.js
+++ assets/js/BotonAgendarCita.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('whatsappButton').addEventListener('click', function() {
         const base_url = "https://api.whatsapp.com/send";
         const phone = "34603122403";  // Número de teléfono del negocio
-        const message = `Hola, me gustaría agendar una cita. Por favor, indíquenme la disponibilidad y los pasos a seguir. ¡Gracias!`;
+        const pageUrl = window.location.href;  // URL de la página actual
+        const message = `Hola, me gustaría agendar una cita. Por favor, indíquenme la disponibilidad y los pasos a seguir. ¡Gracias! Más información: ${pageUrl}`;
 
         // Generar la URL codificada
         const url = `${base_url}?phone=${phone}&text=${encodeURIComponent(message)}`;
